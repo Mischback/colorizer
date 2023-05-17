ba58da35ca50f0a820426036512537431d9ae99e
Mischback <dev@mischback.de>
2023-05-17 17:48:39 +0200

    Add terser plugin to rollup

--- .rollup.config.js
+++ .rollup.config.js
@@ -1,14 +1,24 @@
+import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
+
+/* TODO: "es" or "iife"?!
+ *
+ * https://rollupjs.org/configuration-options/#output-format
+ */
+const outputFormat = "es";
 
 export default {
   input: "src/script/index.ts",
-  output: {
-    file: "dist/assets/colorizer.js",
-    /* TODO: "es" or "iife"?!
-     *
-     * https://rollupjs.org/configuration-options/#output-format
-     */
-    format: "es",
-  },
+  output: [
+    {
+      file: "dist/assets/colorizer.js",
+      format: outputFormat,
+    },
+    {
+      file: "dist/assets/colorizer.min.js",
+      format: outputFormat,
+      plugins: [terser()],
+    },
+  ],
   plugins: [typescript()],
 };
