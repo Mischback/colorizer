fff42663358f9882c97c999ffc3bc7edc9cd8bf9
Mischback <dev@mischback.de>
2023-05-17 22:26:19 +0200

    Create utility module

--- src/script/utility/index.ts
+++ src/script/utility/index.ts
@@ -1,3 +1,26 @@
-export function foo() {
-  console.log("bar");
+// SPDX-FileCopyrightText: 2023 Mischback
+// SPDX-License-Identifier: MIT
+// SPDX-FileType: SOURCE
+
+/**
+ * Get a DOM element by a given query.
+ *
+ * This is a thin wrapper around ``querySelector()``, which throws an ``Error``
+ * if no element could be found.
+ */
+export function getDomElement(
+  root: Element | Document | null,
+  query: string
+): Element {
+  // Start at ``document``, if ``root`` is unspecified
+  if (root === null) {
+    root = document;
+  }
+
+  const tmp = root.querySelector(query);
+  if (tmp === null) {
+    throw new Error(`Missing required DOM element with query '${query}'`);
+  }
+
+  return tmp;
 }
