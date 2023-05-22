c9f227639de8a2191743191f7ef6bca9339736e0
Mischback <dev@mischback.de>
2023-05-22 08:30:36 +0200

    Get all DOM elements for ColorFormInputRgb class

--- src/script/colorizer/interface/color_form/input_methods.ts
+++ src/script/colorizer/interface/color_form/input_methods.ts
@@ -31,12 +31,12 @@ class ColorFormInputRgb
 {
   private static fieldsetId = "color-form-rgb";
   private fieldset: HTMLFieldSetElement;
-  // private inputTextRed: HTMLInputElement;
-  // private inputSliderRed: HTMLInputElement;
-  // private inputTextGreen: HTMLInputElement;
-  // private inputSliderGreen: HTMLInputElement;
-  // private inputTextBlue: HTMLInputElement;
-  // private inputSliderBlue: HTMLInputElement;
+  private inputTextRed: HTMLInputElement;
+  private inputSliderRed: HTMLInputElement;
+  private inputTextGreen: HTMLInputElement;
+  private inputSliderGreen: HTMLInputElement;
+  private inputTextBlue: HTMLInputElement;
+  private inputSliderBlue: HTMLInputElement;
 
   constructor() {
     super();
@@ -48,7 +48,32 @@ class ColorFormInputRgb
         `#${(this.constructor as typeof ColorFormInputRgb).fieldsetId}`
       )
     );
+    this.inputTextRed = <HTMLInputElement>(
+      getDomElement(this.fieldset, ".component-red > input[type=text]")
+    );
+    this.inputSliderRed = <HTMLInputElement>(
+      getDomElement(this.fieldset, ".component-red > input[type=range]")
+    );
+    this.inputTextGreen = <HTMLInputElement>(
+      getDomElement(this.fieldset, ".component-green > input[type=text]")
+    );
+    this.inputSliderGreen = <HTMLInputElement>(
+      getDomElement(this.fieldset, ".component-green > input[type=range]")
+    );
+    this.inputTextBlue = <HTMLInputElement>(
+      getDomElement(this.fieldset, ".component-blue > input[type=text]")
+    );
+    this.inputSliderBlue = <HTMLInputElement>(
+      getDomElement(this.fieldset, ".component-blue > input[type=range]")
+    );
+
     console.debug(this.fieldset);
+    console.debug(this.inputTextRed);
+    console.debug(this.inputSliderRed);
+    console.debug(this.inputTextGreen);
+    console.debug(this.inputSliderGreen);
+    console.debug(this.inputTextBlue);
+    console.debug(this.inputSliderBlue);
   }
 
   public getColor(): void {
