5cece2ed143bc852cc852237b5f962196ebee966
Mischback <dev@mischback.de>
2023-05-24 17:05:33 +0200

    Keep track of the latest color

--- src/script/colorizer/interface/color-form/form.ts
+++ src/script/colorizer/interface/color-form/form.ts
@@ -2,16 +2,23 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileType: SOURCE
 
-import { getDomElement } from "../../../utility";
 import { getColorFormInput, IColorFormInputMethod } from "./input-methods";
+import { ColorizerColor } from "../../lib/color";
+import { getDomElement } from "../../../utility";
 
 // This type describes the ``ColorForm.receiveColor()`` method.
-export type TColorFormReceiverCallback = (foo: string) => void;
+export type TColorFormReceiverCallback = (color: ColorizerColor) => void;
 
 export class ColorForm {
   private static formId = "color-form";
   private form: HTMLFormElement;
   private inputMethods: IColorFormInputMethod[] = [];
+  // Initialization of color **is done** in the ``constructor()`` by calling
+  // ``receiveColor()``.
+  //
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore: strictPropertyInitialization
+  private color: ColorizerColor;
 
   constructor() {
     this.form = <HTMLFormElement>(
@@ -23,14 +30,21 @@ export class ColorForm {
       getColorFormInput("rgb", this.receiveColor.bind(this))
     );
 
+    // Set an initial color for the form and all input methods
+    this.receiveColor(ColorizerColor.fromRgb(0, 0, 0));
+
     // TODO: Remove debug statements
     console.debug(this.form);
     console.debug(this.inputMethods);
   }
 
+  public getColor(): ColorizerColor {
+    return this.color;
+  }
+
   // TODO: Heavily work in progress here!
-  private receiveColor(foo: string): void {
-    console.info(foo);
-    console.info(this.form);
+  private receiveColor(color: ColorizerColor): void {
+    console.info(color);
+    this.color = color;
   }
 }
