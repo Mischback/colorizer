32b43b10740aefdb0b453a5b824a8ad1d3bc9b5a
Mischback <dev@mischback.de>
2023-06-13 22:02:53 +0200

    Refactor database setup

--- src/script/colorizer/engine/database.ts
+++ src/script/colorizer/engine/database.ts
@@ -3,54 +3,66 @@
 // SPDX-FileType: SOURCE
 
 import { openDB } from "idb";
+import type { IColorizerPaletteItem } from "./palette";
 import type { IDBPDatabase, DBSchema } from "idb";
-import type { ColorizerColor } from "../lib/color";
 
-interface ColorizerDBSchema extends DBSchema {
-  colorizer: {
+interface ColorizerDbSchema extends DBSchema {
+  palette: {
     key: string;
-    value: {
-      paletteItemId: string;
-      sorting: number;
-      color: ColorizerColor;
-    };
+    value: IColorizerPaletteItem;
+    indexes: { sorted: number };
   };
 }
+const ColorizerDbSchemaVersion = 1;
 
 export class ColorizerDatabase {
   private dbName: string;
-  private dbVer: number;
+  private dbVersion: number;
   // @ts-expect-error TS6133 value never read
   private db;
 
-  public constructor(dbName = "colorizer", dbVer = 1) {
+  public constructor(
+    dbName = "colorizer",
+    dbVersion: number = ColorizerDbSchemaVersion
+  ) {
     if (!("indexedDB" in window)) {
       throw new Error("Fatal! No support for IndexedDB!");
     }
 
     this.dbName = dbName;
-    this.dbVer = dbVer;
+    this.dbVersion = dbVersion;
 
-    this.db = openDB<ColorizerDBSchema>(this.dbName, this.dbVer, {
+    this.db = openDB<ColorizerDbSchema>(this.dbName, this.dbVersion, {
       upgrade: this.setupDatabase.bind(this),
     });
   }
 
   private setupDatabase(
-    upgradeDb: IDBPDatabase<ColorizerDBSchema>,
+    upgradeDb: IDBPDatabase<ColorizerDbSchema>,
     oldVersion: number,
     newVersion: number | null
   ): void {
-    console.log("setupDatabase()");
-    console.debug(`oldVersion: ${oldVersion}`);
-    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-    console.debug(`newVersion: ${newVersion}`);
+    console.log(
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      `Perform setup of database (from v${oldVersion} to v${newVersion})`
+    );
 
-    /* eslint-disable @typescript-eslint/no-unused-vars */
-    // @ts-expect-error TS6133 value never read
-    const colorizerObjectStore = upgradeDb.createObjectStore("colorizer", {
-      keyPath: "paletteItemId",
-    });
-    /* eslint-enable @typescript-eslint/no-unused-vars */
+    // Apply versioning the to DbSchema!
+    //
+    // This kluge is based on https://web.dev/indexeddb/#versioning
+    // It (deliberatly) falls through the cases, kind of applying database
+    // migrations.
+    //
+    // ``eslint`` complains, if variables/constants are declared in ``case``
+    // clauses, so the ``paletteStore`` is declared outside of the ``switch``
+    // block (no-case-declarations).
+    let paletteStore;
+    switch (oldVersion) {
+      case 0:
+        paletteStore = upgradeDb.createObjectStore("palette", {
+          keyPath: "paletteItemId",
+        });
+        paletteStore.createIndex("sorted", "sorting");
+    }
   }
 }
