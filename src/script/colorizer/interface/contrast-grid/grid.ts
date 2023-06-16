3f70900991ab342f8fb630df7573e3e2c6f43007
Mischback <dev@mischback.de>
2023-06-16 13:25:21 +0200

    [incomplete] Building the contrast grid

--- src/script/colorizer/interface/contrast-grid/grid.ts
+++ src/script/colorizer/interface/contrast-grid/grid.ts
@@ -2,8 +2,7 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileType: SOURCE
 
-import { luminanceFromRgb } from "./calculus";
-import { roundToPrecision } from "../../../utility";
+import { getDomElement } from "../../../utility";
 import type {
   ColorizerPalette,
   ColorizerPaletteItem,
@@ -11,6 +10,7 @@ import type {
 import type { IColorizerPaletteObserver } from "../../lib/types";
 
 export class ColorizerContrastGrid implements IColorizerPaletteObserver {
+  private gridTable: HTMLTableElement;
   private palette: ColorizerPalette;
 
   public constructor(palette: ColorizerPalette) {
@@ -18,6 +18,13 @@ export class ColorizerContrastGrid implements IColorizerPaletteObserver {
     // this instance as an *Observer*.
     this.palette = palette;
     this.palette.addPaletteObserver(this);
+
+    // Get the required DOM elements
+    this.gridTable = <HTMLTableElement>(
+      getDomElement(null, "#contrast-grid table")
+    );
+
+    console.debug(this.gridTable);
   }
 
   /**
@@ -29,19 +36,32 @@ export class ColorizerContrastGrid implements IColorizerPaletteObserver {
    * acts as an *Observer* to the ``ColorizerPalette`` *Observable*.
    */
   public update(palette: ColorizerPaletteItem[]): void {
-    // FIXME: The current code is meant to research #34!
     palette.forEach((item) => {
       console.log(item);
-      const rgb = item.color.toRgb();
-      const xyz = item.color.toJSON();
-      const luminanceCalc = luminanceFromRgb(rgb.r, rgb.g, rgb.b);
 
-      console.log(
-        `calc: ${luminanceCalc}; Y: ${xyz.y}; diff: ${roundToPrecision(
-          Math.abs(luminanceCalc - xyz.y),
-          3
-        )}`
+      this.gridTable.appendChild(
+        this.generateGridRow(item, palette)
       );
     });
+  }
+
+  private generateGridRow(
+    rowItem: ColorizerPaletteItem,
+    palette: ColorizerPaletteItem[]
+  ): HTMLTableRowElement {
+    const template = <HTMLTemplateElement>(
+      document.getElementById("tpl-grid-row")
+    );
+
+    const tableRow = (
+      template.content.firstElementChild as HTMLTableRowElement
+    ).cloneNode(true) as HTMLTableRowElement;
+
+    const itemColor = rowItem.color.toJSON();
+
+    tableRow.setAttribute("palette-item-id", rowItem.paletteItemId);
+    tableRow.style.cssText = `--row-color-x: ${itemColor.x}; --row-color-y: ${itemColor.y}; --row-color-z: ${itemColor.z};`;
+
+    return tableRow;
   }
 }
