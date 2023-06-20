144045d5d274deb4c39753bdaca960c655a4cf81
Mischback <dev@mischback.de>
2023-06-20 21:57:26 +0200

    Make DragToOrder basically work

--- src/script/utility/drag-to-order.ts
+++ src/script/utility/drag-to-order.ts
@@ -2,14 +2,31 @@
 // SPDX-License-Identifier: MIT
 // SPDX-FileType: SOURCE
 
+class ItemNotFoundError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "ItemNotFoundError";
+  }
+}
+
 export class DragToOrder {
   private container: HTMLElement;
-  private itemQuery: string;
   private containerMutationObserver;
+  // @ts-expect-error TS6133 value never read
+  private draggedItem: HTMLElement | undefined;
+  private dropZone: HTMLElement | undefined;
+  private itemQuery: string;
+  private newIndex: number;
+  private oldIndex: number;
 
   constructor(container: HTMLElement, draggableItemQuery: string) {
     this.container = container;
     this.itemQuery = draggableItemQuery;
+
+    this.draggedItem = undefined;
+    this.oldIndex = -1;
+    this.dropZone = undefined;
+    this.newIndex = -1;
 
     this.container.addEventListener(
       "dragstart",
@@ -53,6 +70,20 @@ export class DragToOrder {
     item.setAttribute("draggable", "true");
   }
 
+  private getItemIndex(item: HTMLElement): number {
+    if (item === this.container) {
+      throw new ItemNotFoundError("Item not found in container");
+    }
+
+    const itemIndex = Array.prototype.indexOf.call(this.getItems(), item);
+
+    if (itemIndex === -1) {
+      return this.getItemIndex(<HTMLElement>item.parentNode);
+    }
+
+    return itemIndex;
+  }
+
   /**
    * Get a list of *draggable items*.
    *
@@ -65,32 +96,73 @@ export class DragToOrder {
 
   private handlerDragStart(evt: DragEvent): void {
     console.debug("handlerDragStart()");
-    console.debug(evt);
+    // console.debug(evt);
+
+    if (this.container.contains(evt.target as Node) === false) {
+      return;
+    }
+
+    // @ts-expect-error TS18047 Might be ``null``... Nope!
+    evt.dataTransfer.effectAllowed = "move";
+
+    // FIXME: Make the class name configurable!
+    // evt.target.classList.add("currently-dragged");
+
+    this.draggedItem = <HTMLElement>evt.target;
+    this.oldIndex = this.getItemIndex(<HTMLElement>evt.target);
   }
 
   private handlerDragEnd(evt: DragEvent): void {
     console.debug("handlerDragEnd()");
-    console.debug(evt);
+    // console.debug(evt);
+
+    // FIXME: Make the class name configurable!
+    // evt.target.classList.remove("currently-dragged");
   }
 
   private handlerDragOver(evt: DragEvent): void {
     console.debug("handlerDragOver()");
-    console.debug(evt);
+    // console.debug(evt);
+
+    if (this.dropZone !== evt.target) {
+      this.newIndex = this.getItemIndex(<HTMLElement>evt.target);
+      this.dropZone = <HTMLElement>evt.target;
+    }
+
+    if (this.newIndex !== -1 && this.newIndex !== this.oldIndex) {
+      evt.preventDefault();
+    }
   }
 
   private handlerDragDrop(evt: DragEvent): void {
     console.debug("handlerDragDrop()");
-    console.debug(evt);
+    // console.debug(evt);
+
+    // FIXME: Make the class name configurable!
+    // this.dropZone.classList.remove("drop-target-hover");
+
+    console.info(`oldIndex: ${this.oldIndex}, newIndex: ${this.newIndex}`);
+
+    this.draggedItem = undefined;
+    this.oldIndex = -1;
+    this.dropZone = undefined;
+    this.newIndex = -1;
   }
 
   private handlerDragEnter(evt: DragEvent): void {
     console.debug("handlerDragEnter()");
-    console.debug(evt);
+    // console.debug(evt);
+
+    // FIXME: Make the class name configurable!
+    // evt.target.classList.add("drop-target-hover");
   }
 
   private handlerDragLeave(evt: DragEvent): void {
     console.debug("handlerDragLeave()");
-    console.debug(evt);
+    // console.debug(evt);
+
+    // FIXME: Make the class name configurable!
+    // this.dropZone.classList.remove("drop-target-hover");
   }
 
   /**
