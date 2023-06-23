ceb354a4fd9bd7cbfa00e0982749723f3cc347fd
Mischback <dev@mischback.de>
2023-06-23 10:33:40 +0200

    Provide general implementation of notifications

--- src/script/utility/notifications.ts
+++ src/script/utility/notifications.ts
@@ -3,10 +3,48 @@
 // SPDX-FileType: SOURCE
 
 export class NotificationEngine {
+  container: HTMLElement;
+
   constructor(container: HTMLElement | null) {
     if (container === null) {
       throw new Error("No container provided");
     }
-    console.debug("launched NotificationEngine");
+    this.container = container;
+  }
+
+  public addMessage(message: string, timeout: number | false = 1000) {
+    const notification = document.createElement("section");
+
+    const messageBody = document.createElement("p");
+    messageBody.innerHTML = message;
+    notification.appendChild(messageBody);
+
+    this.container.appendChild(notification);
+
+    if (timeout !== false) {
+      // Trigger automatic removal of the notification with a timeout
+      setTimeout(() => {
+        this.removeMessage(notification);
+      }, timeout);
+    } else {
+      // Create a button to close the notification!
+      const dismiss = document.createElement("button");
+      dismiss.setAttribute("type", "button");
+      dismiss.innerHTML = "close";
+      dismiss.addEventListener("click", () => {
+        this.removeMessage(notification);
+      });
+      notification.appendChild(dismiss);
+    }
+  }
+
+  private removeMessage(notification: HTMLElement): void {
+    // FIXME: Better add a CSS class here. Incorporate transition!
+    // TODO: CSS class name should be configurable (with default value)
+    // FIXME: Make the fade-out interval configurable!
+    notification.setAttribute("style", "opacity: 0.5");
+    setTimeout(() => {
+      this.container.removeChild(notification);
+    }, 250);
   }
 }
