8ba0e98becb188587344fd895ac8290bc5fca473
Mischback <dev@mischback.de>
2023-06-30 05:46:43 +0200

    Create tabbed-interface module

---
+++ src/script/utility/tabbed-interface.ts
@@ -0,0 +1,10 @@
+// SPDX-FileCopyrightText: 2023 Mischback
+// SPDX-License-Identifier: MIT
+// SPDX-FileType: SOURCE
+
+export class TabbedInterface {
+  constructor(tabContainer: HTMLElement) {
+    console.debug("TabbedInterface");
+    console.debug(tabContainer);
+  }
+}
